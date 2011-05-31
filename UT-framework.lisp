b6e2913780e259157add314174a5469070be2d72
arunkn <trybeingarun@gmail.com>
2011-05-31 07:22:18 +0530

    UT-framework ------------ Finished writing UT framework. Added documentation string to with-gensyms macro.

--- UT-framework.lisp
+++ UT-framework.lisp
@@ -1,39 +1,51 @@
+(declaim (optimize (speed 0) (safety 3) (debug 3)))
+
 (load "utility.lisp")
 
 (defvar *test-name* nil)
 
 (defun report-result (result form)
+  "Report the results of a single test case."
   (format t "~:[FAIL~;PASS~] ... ~a: ~a~%" result *test-name* form)
   result)
 
 (defmacro combine-results (&body forms)
+  "Combine the results (as booleans) of evaluating 'forms' in order."
   (with-gensyms (result)
     `(let ((,result t))
        ,@(loop for f in forms
 	       collect `(unless ,f (setf ,result nil)))
        ,result)))
 
-
 (defmacro check-result (&body forms)
+  "Run each expression in 'forms' as a test case"
   `(combine-results
      ,@(loop for f in forms
 	     collect `(report-result ,f ',f))))
 
-(defun test-+ ()
-  (let ((*test-name* 'test-+))
-    (check-result
-      (= (+ 1 1) 2)
-      (= (+ 2 2) 4)
-      (= (+ -2 -5) -7))))
+(defmacro deftest (name parameters &body body)
+  "Define a test function. Within each test function call other test functions
+or use 'check' to run individual test case."
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
 
-(defun test-* ()
-  (let ((*test-name* 'test-*))
-    (check-result
-      (= (* 1 1) 1)
-      (= (* 2 2) 4)
-      (= (* -2 -5) 12))))
+;;; Example usage of the framework
 
-(defun test-arithmetic ()
+(deftest test-+ ()
+  (check-result
+    (= (+ 1 1) 2)
+    (= (+ 2 2) 4)))
+
+(deftest test-* ()
+  (check-result
+    (= (* 1 1) 1)
+    (= (* 5 5) 10)))
+
+(deftest test-arithmetic ()
   (combine-results
-    (test-*)
-    (test-+)))
+    (test-+)
+    (test-*)))
+
+(deftest test-math ()
+  (test-arithmetic))
--- utility.lisp
+++ utility.lisp
@@ -1,4 +1,7 @@
+(declaim (optimize (speed 0) (safety 3) (debug 3)))
+
 (defmacro with-gensyms ((&rest names) &body body)
+  "Generate gensym for the given 'names' and make it available within the form."
   `(let ,(loop for n in names
 	       collect `(,n (gensym)))
      ,@body))
