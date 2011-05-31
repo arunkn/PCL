73a111746f53bb670889cc60853a4d56a836bc48
arunkn <trybeingarun@gmail.com>
2011-05-31 06:37:26 +0530

    UT-framework ------------ The test cases were not returning success or failure (they were only printing results). Fixed it and also combined the return values of all tests so that we know if the test suite passed or not

--- UT-framework.lisp
+++ UT-framework.lisp
@@ -1,13 +1,25 @@
+(load "utility.lisp")
+
 (defun report-result (result form)
-  (format t "~:[FAIL~;PASS~] ... ~a~%" result form))
+  (format t "~:[FAIL~;PASS~] ... ~a~%" result form)
+  result)
+
+(defmacro combine-results (&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms
+	       collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
 
 (defmacro check-result (&body forms)
-  `(progn
+  `(combine-results
      ,@(loop for f in forms
-	    collect `(report-result ,f ',f))))
+	     collect `(report-result ,f ',f))))
 
 (defun test-+ ()
   (check-result
    (= (+ 1 1) 2)
    (= (+ 2 2) 4)
-   (= (+ -2 -5) 7)))
+   (= (+ -2 -5) -7)))
+
