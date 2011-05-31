d8d870a38831c96588d470e1176d9917a703555d
arunkn <trybeingarun@gmail.com>
2011-05-31 05:58:21 +0530

    UT-framework ------------ Instead of calling check-result for every test changed defmacro for check-result so that it would take multiple tests and evaluate it

--- UT-framework.lisp
+++ UT-framework.lisp
@@ -1,11 +1,13 @@
 (defun report-result (result form)
   (format t "~:[FAIL~;PASS~] ... ~a~%" result form))
 
-(defmacro check-result (form)
-  `(report-result ,form ',form))
+(defmacro check-result (&body forms)
+  `(progn
+     ,@(loop for f in forms
+	    collect `(report-result ,f ',f))))
 
 (defun test-+ ()
-  (check-result (= (+ 1 1) 2))
-  (check-result (= (+ 2 2) 4))
-  (check-result (= (+ -2 -5) 7)))
-
+  (check-result
+   (= (+ 1 1) 2)
+   (= (+ 2 2) 4)
+   (= (+ -2 -5) 7)))
